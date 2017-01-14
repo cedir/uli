1af1803089c520ca3b5003e0d79299bdecd0c58f
Ignacio Aguilera <jiaguilera@gmail.com>
2017-01-13 21:45:13 -0300

    arregla error

--- src/js/app/estudios/estudioEpic.js
+++ src/js/app/estudios/estudioEpic.js
@@ -4,7 +4,7 @@ import { getJSON } from 'jquery';
 import moment from 'moment';
 
 const baseUrl = 'http://localhost:8000';
-const today = () => moment('2016-12-29T00:00:00-03:00').add(-1, 'days').format('YYYY-MM-DD');
+const today = () => moment().add(-1, 'days').format('YYYY-MM-DD');
 
 export const estudioEpic = function(action$) {
   return action$.ofType("FETCH_ESTUDIOS_DIARIOS")
