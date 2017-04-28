faae68c3743a95dbee4919db774b6e9887d837ca
Walter Brunetti <walterbrunetti@gmail.com>
2017-04-28 19:33:21 -0300

    Build app for production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,15 +1,17 @@
 import webpack from 'webpack';
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  __DEV__: false
+};
 
 export default {
   debug: true,
-  devtool: 'inline-source-map',  // cheap-module-eval-source-map --> cory lo usa, probar si anda mejor o investigar.
+  devtool: 'source-map',
   noInfo: false,
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/js/main')
-  ],
+  entry: path.resolve(__dirname, 'src/js/main'),
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
@@ -17,11 +19,17 @@ export default {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './src'
+    contentBase: './dist'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin(GLOBALS),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+
+
+    // TODO: y esto??
     new webpack.ProvidePlugin({
         jQuery: 'jquery',
         $: 'jquery'
@@ -30,7 +38,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
       {test: /(\.less)$/, loaders: ['style', 'css', 'less']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?prefix=font/&limit=5000'},
