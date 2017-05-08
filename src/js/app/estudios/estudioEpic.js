8c502d1e408895750cc2de48ae2de74e63bea619
José Ignacio Aguilera <jiaguilera@gmail.com>
2017-05-08 18:54:42 -0300

    rango de fechas en estudios

--- src/js/app/estudios/estudioEpic.js
+++ src/js/app/estudios/estudioEpic.js
@@ -1,15 +1,11 @@
 import Rx from 'rxjs';
 
-import { getJSON } from 'jquery';
-import moment from 'moment';
-
 const baseUrl = 'http://localhost:8000';
-const today = () => moment().add(-1, 'days').format('YYYY-MM-DD');
 
 export const estudioEpic = function(action$) {
   return action$.ofType("FETCH_ESTUDIOS_DIARIOS")
     .mergeMap(action =>
-      Rx.Observable.ajax.getJSON(`${baseUrl}/api/estudio/?fecha_desde=${ today() }`)
+      Rx.Observable.ajax.getJSON(`${baseUrl}/api/estudio/?fecha_desde=${action.fecha.fechaDesde}&fecha_hasta=${action.fecha.fechaHasta}`)
         .map(data => ({ type: 'LOAD_ESTUDIOS_DIARIOS', data }))
         .takeUntil(action$.ofType("CANCEL_ESTUDIOS_DIARIOS"))
     );
