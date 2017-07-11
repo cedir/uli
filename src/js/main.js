f5cdc0bb0751a2e4f0c15713b07ebc1989c30c8d
matiasstrizzolo <matiasstrizzolo@gmail.com>
2017-07-11 07:12:07 -0300

    Footable dependencies added again

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './app/app';
-import configureStore from './app/configureStore';
 // require('./favicon.ico'); // Tell webpack to load favicon.ico
 
 /* eslint-disable no-unused-vars */
+// Required to load footable plugins begin
+import jQuery from 'jquery';
+import bootstrap from 'bootstrap';
+import slimscroll from 'jquery-slimscroll';
+import metismenu from 'metismenu';
+import FootableJS from './app/layout/footable/footable.all.min';
+// Required to load footable plugins end
+
+import App from './app/app';
+import configureStore from './app/configureStore';
+import inspinia from './app/layout/inspinia';
 import Custom from './../style/style.less';
 import FooTable from './../style/footable.core.css';
 
