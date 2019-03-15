8fe61d104161d0fea287c0911532025c3bdcdfe1
Walter Brunetti <walterbrunetti@gmail.com>
2019-03-15 19:56:50 -0300

    AMR soporte digital

--- src/js/presentaciones/api.js
+++ src/js/presentaciones/api.js
@@ -24,7 +24,29 @@ export function getPresentacionFormatoOsde(presentacion) {
     get(url, customHeader, 'string')
         .subscribe(
             (ajaxResponse) => {
-                const fileName = `presentacion_${obraSocial.nombre}_${fecha}.txt`;
+                const fileName = `presentacion_${obraSocial.nombre}_${fecha}_format_OSDE.txt`;
+                saveFile(fileName, ajaxResponse.response);
+            },
+            reason => (
+                store.dispatch({ type: ADD_ALERT, alert: createAlert(JSON.parse(reason.response).error, 'danger') })),
+        );
+}
+
+export function getPresentacionFormatoAMR(presentacion) {
+    const {
+        id,
+        obra_social: obraSocial,
+        fecha } = presentacion;
+
+    const url = `/api/presentacion/${id}/get_detalle_amr`;
+    const customHeader = {
+        'Content-Type': 'text/plain',
+    };
+
+    get(url, customHeader, 'string')
+        .subscribe(
+            (ajaxResponse) => {
+                const fileName = `presentacion_${obraSocial.nombre}_${fecha}_format_AMR.txt`;
                 saveFile(fileName, ajaxResponse.response);
             },
             reason => (
