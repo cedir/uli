ac955d0b2ed2788b23d49d6f48acd1cac578c65c
davidtissera1 <davidtissera@outlook.com>
2020-01-29 14:55:57 -0300

    Se quito boton Ir en pantalla Presentaciones a obra social y se modifico ruta

--- src/js/presentaciones/components/PresentacionesObraSocialPage.js
+++ src/js/presentaciones/components/PresentacionesObraSocialPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import SearchPresentacionesObraSocial from './SearchPresentacionesObraSocial';
 import PresentacionesObraSocialList from './PresentacionesObraSocialList';
 
-const PresentacionesObraSocialPage = () => (
+const PresentacionesObraSocialPage = props => (
     <div>
         <h1>Presentaciones a obra social</h1>
-        <SearchPresentacionesObraSocial />
+        <SearchPresentacionesObraSocial
+          history={ props.history }
+        />
         <PresentacionesObraSocialList />
     </div>
 );
+
+const { object } = PropTypes;
+
+PresentacionesObraSocialPage.propTypes = {
+    history: object.isRequired,
+};
 
 export default PresentacionesObraSocialPage;
