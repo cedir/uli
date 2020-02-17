323f208feee92ca2fb172342d68b72f06c9ef158
davidtissera1 <davidtissera@outlook.com>
2020-02-17 13:03:02 -0300

    Se reescribieron los componentes de Login y App con Functional Components

--- src/js/login/components/Login.js
+++ src/js/login/components/Login.js
@@ -1,106 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Button, FormGroup, FormControl }
     from 'react-bootstrap/dist/react-bootstrap';
 import ConditionalComponent from '../../utilities/ConditionalComponent';
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
-        this.setUsername = this.setUsername.bind(this);
-        this.setPassword = this.setPassword.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
-    }
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unused-prop-types */
 
-    componentWillMount() {
-        if (this.props.token) {
-            this.props.history.push('/');
+function Login(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [history, setHistory] = useState(props.history);
+
+    useEffect(() => {
+        if (props.token) {
+            setHistory(history.push('/'));
         }
-    }
+    });
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.token) {
-            this.props.history.push('/');
-        }
-    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        props.authUser(username, password);
+    };
 
-    setUsername(evt) {
-        const username = evt.target.value;
-        this.setState({ username });
-    }
-
-    setPassword(evt) {
-        const password = evt.target.value;
-        this.setState({ password });
-    }
-
-    submitHandler(evt) {
-        evt.preventDefault();
-        const { username, password } = this.state;
-        this.props.authUser(username, password);
-    }
-
-    loginErrorComponent() {
+    const loginErrorComponent = () => {
         return (
             <p className='m-t'>
                 <small style={ { color: 'red', fontSize: '16px' } }>username o password incorrecto</small>
             </p>
         );
-    }
+    };
 
-    render() {
-        return (
-            <div className='middle-box text-center loginscreen animated fadeInDown'>
+    return (
+        <div className='middle-box text-center loginscreen animated fadeInDown'>
+            <div>
                 <div>
-                    <div>
-
-                        <h1 className='logo-name'>IN+</h1>
-
-                    </div>
-                    <h3>Bienvenida/o a ULI</h3>
-                    <p>
-                        App desarrollada por C.E.Di.R
-                    </p>
-                    <p>Logueate para comenzar.</p>
-                    <form className='m-t' onSubmit={ this.submitHandler } >
-                        <FormGroup controlId='username' >
-                            <FormControl
-                              type='text'
-                              value={ this.state.username }
-                              placeholder='Username'
-                              onChange={ this.setUsername }
-                            />
-                        </FormGroup>
-                        <FormGroup controlId='password' >
-                            <FormControl
-                              type='password'
-                              value={ this.state.password }
-                              placeholder='Password'
-                              onChange={ this.setPassword }
-                            />
-                        </FormGroup>
-                        <Button
-                          type='submit'
-                          bsStyle='primary'
-                          bsSize='large'
-                          block
-                        >
-                            Login
-                        </Button>
-                    </form>
-                    <ConditionalComponent
-                      component={ this.loginErrorComponent }
-                      display={ this.props.loginError }
-                    />
+                    <h1 className='logo-name'>IN+</h1>
                 </div>
+                <h3>Bienvenida/o a ULI</h3>
+                <p>
+                    App desarrollada por C.E.Di.R
+                </p>
+                <p>Logueate para comenzar.</p>
+                <form className='m-t' onSubmit={ submitHandler } >
+                    <FormGroup controlId='username' >
+                        <FormControl
+                          type='text'
+                          value={ username }
+                          placeholder='Username'
+                          onChange={ e => setUsername(e.target.value) }
+                        />
+                    </FormGroup>
+                    <FormGroup controlId='password' >
+                        <FormControl
+                          type='password'
+                          value={ password }
+                          placeholder='Password'
+                          onChange={ e => setPassword(e.target.value) }
+                        />
+                    </FormGroup>
+                    <Button
+                      type='submit'
+                      bsStyle='primary'
+                      bsSize='large'
+                      block
+                    >
+                        Login
+                    </Button>
+                </form>
+                <ConditionalComponent
+                  component={ loginErrorComponent }
+                  display={ props.loginError }
+                />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 const { func, object, string, bool } = PropTypes;
