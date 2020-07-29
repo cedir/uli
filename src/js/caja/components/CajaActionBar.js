95ceea067c15c0a8e387c20a3c61480701d4fe2f
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-07-29 17:34:23 -0300

    Se centra el monto acumulado

--- src/js/caja/components/CajaActionBar.js
+++ src/js/caja/components/CajaActionBar.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Well, Grid, Col } from 'react-bootstrap/dist/react-bootstrap';
+import { Button, Well, Grid, Col, Row } from 'react-bootstrap/dist/react-bootstrap';
 
 export default class CajaActionBar extends Component {
     render() {
         return (
             <Grid>
-                <Col md={ 2 } mdOffset={ 2 }>
-                    <Well>Monto acumulado: { this.props.montoAcumulado }</Well>
-                </Col>
-                <Col md={ 2 } mdOffset={ 6 }>
-                    <Button onClick={ this.props.openSearchCajaModal }>
-                        Buscar movimiento
-                    </Button>
-                </Col>
+                <Row>
+                    <Col md={ 10 }>
+                        <Row>
+                            <Col mdOffset={ 2 } md={ 3 } xs={ 5 } xsOffset={ 2 }>
+                                <Well>Monto acumulado: { this.props.montoAcumulado }</Well>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={ 2 }>
+                        <Button onClick={ this.props.openSearchCajaModal }>
+                            Buscar movimiento
+                        </Button>
+                    </Col>
+                </Row>
             </Grid>
         );
     }
