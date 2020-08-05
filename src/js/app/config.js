caab87f95e70d5fbf3db38afb448ed74601912fd
QPotato <federicobadaloni@hotmail.com>
2020-08-05 13:22:06 -0300

    se saca de control de versiones el archivo de configuracion

--- src/js/app/config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    baseUrl: 'http://127.0.0.1:8000',
-    // baseUrl: 'https://staging.cedir.com.ar',
-    // baseUrl: 'http://localhost:8000',
-};
-
-export const REDUX_LOGGER_STATUS = 0;
