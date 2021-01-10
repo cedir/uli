52da30951ac5caa1569449c1b4175566f7d9cc6a
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-01-10 15:55:50 -0300

    Se convierte de clase a funcion el componente EstudiosListTable para poder utilizar react-to-print

--- src/js/estudio/components/EstudiosListTable.js
+++ src/js/estudio/components/EstudiosListTable.js
@@ -1,49 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Table }
-    from 'react-bootstrap/dist/react-bootstrap';
-
+import { Table } from 'react-bootstrap/dist/react-bootstrap';
 import EstudioListTableRow from './EstudioListTableRow';
 
-class EstudiosListTable extends React.Component {
-    constructor(props) {
-        super(props);
+function EstudiosListTable({ estudios, history }) {
+    const navigateToEstudioDetail = estudioId => history.push(`/estudios/detail/${estudioId}`);
 
-        this.navigateToEstudioDetail = this.navigateToEstudioDetail.bind(this);
-    }
-
-    navigateToEstudioDetail(estudioId) {
-        this.props.history.push(`/estudios/detail/${estudioId}`);
-    }
-
-    render() {
-        return (
-            <div>
-                <Table striped responsive style={ { marginTop: '20px' } }>
-                    <thead>
-                        <tr>
-                            <th>Fecha</th>
-                            <th>Nombre Paciente</th>
-                            <th>Obra Social</th>
-                            <th>Tipo de estudio</th>
-                            <th>Medico actuante</th>
-                            <th>Medico solicitante</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.props.estudios.map(estudio => (
-                            <EstudioListTableRow
-                              key={ estudio.id }
-                              estudio={ estudio }
-                              onRowClick={ this.navigateToEstudioDetail }
-                            />
-                        )) }
-                    </tbody>
-                </Table>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Table striped responsive style={ { marginTop: '20px' } }>
+                <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Nombre Paciente</th>
+                        <th>Obra Social</th>
+                        <th>Tipo de estudio</th>
+                        <th>Medico actuante</th>
+                        <th>Medico solicitante</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { estudios.map(estudio => (
+                        <EstudioListTableRow
+                          key={ estudio.id }
+                          estudio={ estudio }
+                          onRowClick={ navigateToEstudioDetail }
+                        />
+                    )) }
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 const { array, object } = PropTypes;
