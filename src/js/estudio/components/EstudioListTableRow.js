9b7cd4220a50d52c861f9bc1d9a2fb51238b9a3c
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-01-20 06:23:24 -0300

    Refactor EstudioListTableRow

--- src/js/estudio/components/EstudioListTableRow.js
+++ src/js/estudio/components/EstudioListTableRow.js
@@ -6,44 +6,34 @@ function formatDate(dateString) {
     return `${d.getUTCDate()}/${d.getUTCMonth() + 1}/${d.getUTCFullYear()}`;
 }
 
-class EstudioListTableRow extends React.Component {
-    constructor(props) {
-        super(props);
+function EstudioListTableRow({ estudio, onRowClick, showMedicoSolicitante }) {
+    const paciente = estudio.paciente || { nombre: '', apellido: '' };
+    const obraSocial = estudio.obra_social || { nombre: '' };
+    const practica = estudio.practica || { descripcion: '' };
+    const medico = estudio.medico || { nombre: '', apellido: '' };
+    const medicoSolicitante = estudio.medico_solicitante || { nombre: '', apellido: '' };
 
-        this.onRowClick = this.onRowClick.bind(this);
-    }
-    onRowClick() {
-        const estudioId = this.props.estudio.id;
-        this.props.onRowClick(estudioId);
-    }
-    render() {
-        /* If any of this requirements objects are not present in the estudio object,
-        * supply a default one. This situation is exceptional and related with wrong data in db
-        */
-        const paciente = this.props.estudio.paciente || { nombre: '', apellido: '' };
-        const obraSocial = this.props.estudio.obra_social || { nombre: '' };
-        const practica = this.props.estudio.practica || { descripcion: '' };
-        const medico = this.props.estudio.medico || { nombre: '', apellido: '' };
-        const medicoSolicitante = this.props.estudio.medico_solicitante || { nombre: '', apellido: '' };
-
-        return (
-            <tr onClick={ this.onRowClick } style={ { cursor: 'pointer' } }>
-                <td>{ formatDate(this.props.estudio.fecha) }</td>
-                <td>{ `${paciente.apellido}, ${paciente.nombre}` }</td>
-                <td>{ obraSocial.nombre }</td>
-                <td>{ practica.descripcion }</td>
-                <td>{ `${medico.apellido}, ${medico.nombre}` }</td>
-                <td>{ `${medicoSolicitante.apellido}, ${medicoSolicitante.nombre}` }</td>
-            </tr>
-        );
-    }
+    return (
+        <tr
+          onClick={ () => onRowClick(estudio.id) }
+          style={ { cursor: 'pointer' } }
+        >
+            <td>{ formatDate(estudio.fecha) }</td>
+            <td>{ `${paciente.apellido}, ${paciente.nombre}` }</td>
+            <td>{ obraSocial.nombre }</td>
+            <td>{ practica.descripcion }</td>
+            <td>{ `${medico.apellido}, ${medico.nombre}` }</td>
+            {showMedicoSolicitante && <td>{ `${medicoSolicitante.apellido}, ${medicoSolicitante.nombre}` }</td>}
+        </tr>
+    );
 }
 
-const { object, func } = PropTypes;
+const { object, func, bool } = PropTypes;
 
 EstudioListTableRow.propTypes = {
     estudio: object.isRequired,
     onRowClick: func.isRequired,
+    showMedicoSolicitante: bool.isRequired,
 };
 
 export default EstudioListTableRow;
