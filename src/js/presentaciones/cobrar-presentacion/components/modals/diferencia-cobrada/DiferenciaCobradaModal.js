4057a08f86080cb15204401cd139ddc2cb920d29
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-02-11 21:13:09 -0300

    Se intercambian los valores con iva y la diferencia cobrada

--- src/js/presentaciones/cobrar-presentacion/components/modals/diferencia-cobrada/DiferenciaCobradaModal.js
+++ src/js/presentaciones/cobrar-presentacion/components/modals/diferencia-cobrada/DiferenciaCobradaModal.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function DiferenciaCobradaModal({ diferenciaCobrada, diferenciaSinIva }) {
+function DiferenciaCobradaModal({ diferenciaCobrada, diferenciaConIva }) {
     return (
         <React.Fragment>
-            Se ha detectado una diferencia de {diferenciaSinIva}
-            {diferenciaSinIva !== diferenciaCobrada ? ` (con iva ${diferenciaCobrada})` : ''}.
+            Se ha detectado una diferencia de {diferenciaCobrada}
+            {diferenciaConIva !== diferenciaCobrada ? ` (con iva ${diferenciaConIva})` : ''}.
             {' '}¿Quiere crear un comprobante asociado?
         </React.Fragment>
     );
@@ -15,7 +15,7 @@ const { number } = PropTypes;
 
 DiferenciaCobradaModal.propTypes = {
     diferenciaCobrada: number.isRequired,
-    diferenciaSinIva: number.isRequired,
+    diferenciaConIva: number.isRequired,
 };
 
 export default DiferenciaCobradaModal;
