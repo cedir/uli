83a461375e8a5bc5ac9de60015d6ba206ac6fe73
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-03-28 15:23:20 -0300

    Refactor ListadoMovimientosTable

--- src/js/caja/components/ListadoMovimientosTable.js
+++ src/js/caja/components/ListadoMovimientosTable.js
@@ -1,53 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import propTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Table }
     from 'react-bootstrap/dist/react-bootstrap';
 
 import ListadoMovimientosTableRow from './ListadoMovimientosTableRow';
-import './ListadoMovimientosTable.css';
 
-class ListadoMovimientosTable extends Component {
-
-    render() {
-        return (
-            <div className='listado-movimientos'>
-                <Table striped responsive>
-                    <thead>
-                        <tr>
-                            <th>Fecha Movimiento</th>
-                            <th>Tipo Movimiento</th>
-                            <th>Descripcion Movimiento</th>
-                            <th>Monto</th>
-                            <th>Monto acumulado</th>
-                            <th>Fecha estudio</th>
-                            <th>Obra social</th>
-                            <th>Practica</th>
-                            <th>Paciente</th>
-                            <th>Medico</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.props.movimientos.map(movimiento =>
-                                (
-                                    <ListadoMovimientosTableRow
-                                      key={ movimiento.id }
-                                      movimiento={ movimiento }
-                                    />
-                                ))
-                        }
-                    </tbody>
-                </Table>
-            </div>
-        );
-    }
+function ListadoMovimientosTable({ movimientos }) {
+    return (
+        <div className='listado-movimientos'>
+            <Table striped responsive>
+                <thead>
+                    <tr>
+                        <th>Fecha Movimiento</th>
+                        <th>Tipo Movimiento</th>
+                        <th>Descripcion Movimiento</th>
+                        <th>Monto</th>
+                        <th>Monto acumulado</th>
+                        <th>Fecha estudio</th>
+                        <th>Obra social</th>
+                        <th>Practica</th>
+                        <th>Paciente</th>
+                        <th>Medico</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        movimientos.map(movimiento =>
+                            (
+                                <ListadoMovimientosTableRow
+                                  key={ movimiento.id }
+                                  movimiento={ movimiento }
+                                />
+                            ))
+                    }
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
-const { array } = propTypes;
+const { array } = PropTypes;
 
 ListadoMovimientosTable.propTypes = {
     movimientos: array.isRequired,
 };
 
-export default connect(null, null)(ListadoMovimientosTable);
+export default ListadoMovimientosTable;
