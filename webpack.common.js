86dfe49525f0a232072c153c1143edc9e19cf6fe
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-06-05 16:03:02 -0300

    Eliminacion de paquetes innecesarios y actualizacion de webpack junto a sus paquetes

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [path.resolve(__dirname, 'src/js/main')],
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+        })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg|woff(2)?|ttf|eot|svg)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+};
