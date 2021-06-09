e36d603d60932cea8625c822de0189937c4d0494
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-06-09 14:58:23 -0300

    Updated webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,17 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const PUERTO = 3000;
 
 module.exports = merge(common, {
     mode: 'development',
-    output: {
-        path: path.resolve(__dirname, '/dist'),
-        publicPath: '/',
-        filename: 'bundle.js',
-    },
     devServer: {
         contentBase: './src',
         port: PUERTO,
         historyApiFallback: true,
     },
+    plugins: [new ESLintPlugin()],
     module: {
         rules: [
             {
