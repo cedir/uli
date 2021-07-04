c34faa6d8412384bb64d14f59fe7a62ce1d3d384
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-07-04 16:00:32 -0300

    Se borran el imprimir estudio

--- src/js/estudio/components/EstudiosActionBar.js
+++ src/js/estudio/components/EstudiosActionBar.js
@@ -1,44 +1,16 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useReactToPrint } from 'react-to-print';
 import { ButtonToolbar, Button } from 'react-bootstrap/dist/react-bootstrap';
 
 function EstudiosActionBar({
     history,
     setModalOpened,
-    estudiosRef,
-    setPrintMode,
-    printMode,
     fromCaja,
 }) {
-    const onBeforeGetContentResolve = useRef(Promise.resolve);
     const goToCreateEstudio = () => {
         history.push('/estudios/create');
     };
 
-    const estudiosContent = useCallback(
-        () => estudiosRef.current,
-        [estudiosRef.current],
-    );
-
-    const handlePrint = useReactToPrint({
-        content: estudiosContent,
-        onBeforeGetContent: () => new Promise((resolve) => {
-            onBeforeGetContentResolve.current = resolve;
-            setPrintMode(true);
-            resolve();
-        }),
-        onAfterPrint: () => {
-            setPrintMode(false);
-        },
-        removeAfterPrint: true,
-    });
-
-    useEffect(() => {
-        if (printMode && typeof onBeforeGetContentResolve.current === 'function') {
-            onBeforeGetContentResolve.current();
-        }
-    }, [onBeforeGetContentResolve.current, printMode]);
     return (
         <ButtonToolbar>
             <Button
@@ -47,11 +19,10 @@ function EstudiosActionBar({
                 Buscar estudio
             </Button>
             { !fromCaja && <Button
-              onClick={ handlePrint }
+              onClick={ () => console.log('click') }
             >
                 Imprimir
             </Button>}
-            {/* imprimir tira error si no hay estudios */}
             { !fromCaja && <Button
               bsStyle='primary'
               onClick={ goToCreateEstudio }
@@ -67,9 +38,6 @@ const { func, object, bool } = PropTypes;
 EstudiosActionBar.propTypes = {
     history: object.isRequired,
     setModalOpened: func.isRequired,
-    estudiosRef: object,
-    setPrintMode: func.isRequired,
-    printMode: bool.isRequired,
     fromCaja: bool.isRequired,
 };
 
