03f4b0c0e246b7272d1380fa123f5be687d0f5a4
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-07-11 21:22:48 -0300

    Se agregan comentarios y se crea test sobre modificar presentacion

--- cypress/integration/PresentacionTest.js
+++ cypress/integration/PresentacionTest.js
@@ -12,6 +12,7 @@ beforeEach(() => {
 describe('Crear Presentacion', () => {
     /* Setup section */
     beforeEach(() => {
+        /* Create a study and open presentation for the study */
         cy.visit('/estudios/create');
         crearEstudio();
         cy.visit('/presentaciones-obras-sociales');
@@ -23,20 +24,25 @@ describe('Crear Presentacion', () => {
 
     /* Test section */
     it('Crear presentacion con factura funciona', () => {
+        /* Date input */
         cy.get('input[name=date]').type(today);
 
+        /* Comprobante modal */
         cy.contains('button', 'Comprobante').click();
         cy.get('.modal').find('select').eq(0).select('Factura');
         cy.get('.modal').find('select').eq(1).select('A');
         cy.get('.modal').find('select').eq(2).select('CeDIR');
         cy.contains('button', 'Cerrar').click();
 
+        /* Finalizar modal */
         cy.contains('button', 'Finalizar').click();
         cy.get('.modal').find('input[name=periodo]').type('1 MES');
         cy.get('.modal').contains('button', 'Finalizar').should('not.be.disabled').click();
 
+        /* ok confirmation */
         cy.contains('Presentación creada y cerrada con éxito');
 
+        /* Check existence and status */
         cy.visit('/presentaciones-obras-sociales');
         cy.typeAsyncInput('obraSocial', 'OSDE BINARIO');
         cy.clickFirstOption();
@@ -58,14 +64,54 @@ describe('Listado de presentaciones', () => {
         cy.contains('button', 'Buscar').click();
         cy.firstRowType('Pendiente').eq(listadoCols.nroComprobante).invoke('text')
             .then((numero) => {
+                /* Open the first presentation with status "Pendiente" */
                 cy.contains('td', numero).parent().find('td').eq(listadoCols.abrir)
                     .children()
                     .click();
                 cy.get('.modal').contains('button', 'Abrir').click();
+
+                /* Ok confirmation */
                 cy.contains('La presentacion fue abierta exitosamente');
-                cy.contains(numero).should('not.exist');
+
+                /* Check if number of invoice is deleted */
+                cy.contains(new RegExp(`^${numero}$`)).should('not.exist');
                 cy.get('tbody > tr').contains('Abierto');
             });
+    });
+});
+
+describe('Modificar presentacion', () => {
+    /* Test section */
+    beforeEach(() => {
+        cy.contains('button', 'Buscar').click();
+        cy.firstRowType('Abierto').eq(listadoCols.editar).children().click();
+        cy.contains('Estudios cargados correctamente');
+    });
+
+    it('Abrir presentacion funciona correctamente', () => {
+        /* Date input. Delete when the bug was fixed */
+        cy.get('input[name=date]').type('2021-07-10');
+        cy.get('input[name=date]').type(today);
+
+        /* Comprobante modal */
+        cy.contains('button', 'Comprobante').click();
+        cy.get('.modal').find('select').eq(0).select('Liquidacion');
+        cy.contains('button', 'Cerrar').click();
+
+        /* Finalizar modal */
+        cy.contains('button', 'Finalizar').click();
+        cy.get('.modal').find('input[name=periodo]').type('2 MESES');
+        cy.get('.modal').contains('button', 'Finalizar').should('not.be.disabled').click();
+
+        /* Ok confirmation */
+        cy.contains('Presentación actualizada y cerrada con éxito');
+
+        /* Check status */
+        cy.visit('/presentaciones-obras-sociales');
+        cy.typeAsyncInput('obraSocial', 'OSDE BINARIO');
+        cy.clickFirstOption();
+        cy.contains('button', 'Buscar').click();
+        cy.get('tbody > tr').contains('td', 'OSDE BINARIO').parent().contains('td', 'Pendiente');
     });
 });
 
